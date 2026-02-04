db4a3e99cea74d7dfa2fa2293761e6afb295eef9
Pranav Dobhal <dobhalanshu@gmail.com>
2026-02-04 21:57:25 +0530

    Add files via upload

---
+++ 4dsa.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+void reverseArray(int arr[], int n) 
+{
+    int start = 0;
+    int end = n - 1;
+    int temp;
+
+    while (start < end) 
+    {
+        temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        start++;
+        end--;
+    }
+}
+
+int main() 
+{
+    int n;
+    if (scanf("%d", &n) != 1) return 1;
+    
+    int *arr = (int*)malloc(n * sizeof(int));
+    
+    if (arr == NULL) return 1; 
+
+    for (int i = 0; i < n; i++) 
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    reverseArray(arr, n);
+
+    for (int i = 0; i < n; i++) 
+    {
+        printf("%d ", arr[i]);
+    }
+        free(arr);
+    return 0;
+}
