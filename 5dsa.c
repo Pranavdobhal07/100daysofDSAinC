be30b36f9f8db92c1f506d5aa7f1352f058fefb3
Pranav Dobhal <dobhalanshu@gmail.com>
2026-02-05 22:17:11 +0530

    Add files via upload

---
+++ 5dsa.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+int main() 
+{
+    int log1[1000], log2[1000];
+    int p, q;
+
+    scanf("%d", &p);
+    for (int i = 0; i < p; i++) 
+    {
+        scanf("%d", &log1[i]);
+    }
+
+    scanf("%d", &q);
+    for (int i = 0; i < q; i++) 
+    {
+        scanf("%d", &log2[i]);
+    }
+
+    int i = 0, j = 0;
+
+    while (i < p && j < q) 
+    {
+        if (log1[i] < log2[j]) 
+        {
+            printf("%d ", log1[i]);
+            i++;
+        } else 
+        {
+            printf("%d ", log2[j]);
+            j++;
+        }
+    }
+
+    while (i < p)
+     {
+        printf("%d ", log1[i]);
+        i++;
+    }
+
+    while (j < q) 
+    {
+        printf("%d ", log2[j]);
+        j++;
+    }
+
+    return 0;
+}
